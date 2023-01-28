be9198b6106fdb9e1a1d19c58cf1a5969d73d4fb
Örs Vajda <ors0092@gmail.com>
2023-01-28 19:41:40 +0100

    enhancement of core program

--- luxor.py
+++ luxor.py
@@ -1,2 +1,12 @@
-name = input('Please input your name: ')
-print(f"Testing git/github connection. Welcome user {name}! \n added gpg")
+def is_interactive():
+	usr = input('Please input your name: ')
+	print(f"Welcome player {usr}!")
+
+_itc = input("Go with interactive mode? Y/n : ")
+
+if _itc == 'Y':
+	is_interactive()
+elif _itc == 'n':
+	print("We fly without interaction.")
+else:
+	print("Aborting.")
